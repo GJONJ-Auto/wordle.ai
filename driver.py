5d48274dbd36aaf0f1d577fe14f03c0ec28e2689
Nescolindres <nestorcolindres17@gmail.com>
2023-06-21 18:20:57 -0700

    Added function to check if user submitted intial word is valid

--- driver.py
+++ driver.py
@@ -1,3 +1,12 @@
 import random
 
 wordle_list = [""] #This will be where we import the wordle list. Probably should import JSON file.
+
+#Check if the word is valid (User input -> wordle list)
+def is_valid(word):
+    ok = True
+    if len(word) == 5 and word in wordle_list:
+        return
+    else:
+        ok = False
+    return ok
