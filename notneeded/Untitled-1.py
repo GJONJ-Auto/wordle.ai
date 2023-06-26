dc30e7fbf2e2ec0c3325d57725eec61785ff5bb9
JEspinoza501 <juan.espinoza.262@my.csun.edu>
2023-06-25 23:23:32 -0700

    json wordle list creation

--- notneeded/Untitled-1.py
+++ notneeded/Untitled-1.py
@@ -2,19 +2,18 @@ import json
 
 def convert_to_json(input_file, output_file):
     data = []
-    word_id = 1  # Start with word_id = 1
     with open(input_file, 'r') as file:
         for line in file:
             word = line.strip()  # Remove leading/trailing whitespace or newline characters
-            entry = {'word_id': word_id, 'word': word}
+            entry = {'word': word}
             data.append(entry)
-            word_id += 1
+       
     
     with open(output_file, 'w') as file:
         json.dump(data, file, indent=4)
 
 
 # Usage example
-input_file = 'wordle-nyt-allowed-guesses-update-12546.txt'
-output_file = 'wordlelistv2.json'
+input_file = 'wordle_list.txt'
+output_file = 'wordlelistv1.json'
 convert_to_json(input_file, output_file)
